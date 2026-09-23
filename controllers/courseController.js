0f706a8c96f47460f94085bac65f31bdc8c9c7b3
Tatah Withney <164454406+tatahwithney@users.noreply.github.com>
2026-09-23 23:21:27 +0100

    add course controller

---
+++ controllers/courseController.js
@@ -0,0 +1,159 @@
+const Course = require("../models/course");
+
+// Display all courses
+exports.getAllCourses = async (req, res, next) => {
+    try {
+        const courses = await Course.find();
+
+        res.render("courses/index", {
+            courses
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+// Display create course form
+exports.showCreateForm = (req, res) => {
+    res.render("courses/new", {
+        course: {},
+        errors: {}
+    });
+};
+
+// Create a course
+exports.createCourse = async (req, res, next) => {
+    try {
+        const { name, description, duration, instructor, maxStudents } = req.body;
+
+        await Course.create({
+            name,
+            description,
+            duration,
+            instructor,
+            maxStudents
+        });
+
+        // PRG: POST -> Redirect -> GET
+        res.redirect("/courses");
+    } catch (error) {
+        if (error.name === "ValidationError") {
+            const errors = {};
+
+            for (const field in error.errors) {
+                errors[field] = error.errors[field].message;
+            }
+
+            return res.status(400).render("courses/new", {
+                course: req.body,
+                errors
+            });
+        }
+
+        next(error);
+    }
+};
+
+// Display one course
+exports.getCourse = async (req, res, next) => {
+    try {
+        const course = await Course.findById(req.params.id);
+
+        if (!course) {
+            return res.status(404).render("404");
+        }
+
+        res.render("courses/show", {
+            course
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+// Display edit form
+exports.showEditForm = async (req, res, next) => {
+    try {
+        const course = await Course.findById(req.params.id);
+
+        if (!course) {
+            return res.status(404).render("404");
+        }
+
+        res.render("courses/edit", {
+            course,
+            errors: {}
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+// Update a course
+exports.updateCourse = async (req, res, next) => {
+    try {
+        const { name, description, duration, instructor, maxStudents } = req.body;
+
+        const course = await Course.findByIdAndUpdate(
+            req.params.id,
+            {
+                name,
+                description,
+                duration,
+                instructor,
+                maxStudents
+            },
+            {
+                new: true,
+                runValidators: true
+            }
+        );
+
+        if (!course) {
+            return res.status(404).render("404");
+        }
+
+        // PRG: POST -> Redirect -> GET
+        res.redirect(`/courses/${course._id}`);
+    } catch (error) {
+        if (error.name === "ValidationError") {
+            const course = await Course.findById(req.params.id);
+
+            if (!course) {
+                return res.status(404).render("404");
+            }
+
+            const errors = {};
+
+            for (const field in error.errors) {
+                errors[field] = error.errors[field].message;
+            }
+
+            return res.status(400).render("courses/edit", {
+                course: {
+                    ...course.toObject(),
+                    ...req.body
+                },
+                errors
+            });
+        }
+
+        next(error);
+    }
+};
+
+// Delete a course
+exports.deleteCourse = async (req, res, next) => {
+    try {
+        const course = await Course.findByIdAndDelete(req.params.id);
+
+        if (!course) {
+            return res.status(404).render("404");
+        }
+
+        // PRG: POST -> Redirect -> GET
+        res.redirect("/courses");
+    } catch (error) {
+        next(error);
+    }
+};
